dd919add74717f4f2817a30ed8fb61ad0f2b6314
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-16 19:16:52 +0100

    Get human choice done

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-console.log(`Hello World`);
+console.log(`Hello World`);                             //html link test
 
-function getComputerChoice(computer) {
+//Computer
+function getComputerChoice(computer) {                  // Create a random number between 0 and 2
     return computer = Math.floor(Math.random() * 3);
 }
 console.log(getComputerChoice());
 
-if (getComputerChoice() === 0) {
+if (getComputerChoice() === 0) {                        // Assign Rock, Paper or Scissor to random Number
     computer = `Rock`;
 } else if (getComputerChoice() === 1) {
     computer = `Paper`;
 } else {
     computer =`Scissor`;
 }
-
 console.log(computer);
+//  -----
+
+//Human
+function getHumanChoice() {                                                 //Get player choice
+    let human = prompt(`Please choose between Rock, Paper and Scissor.`);   //Let user choose his play
+console.log(human);
+
+        human = human.toLowerCase();                                        //Convert to lower case for compatibility
+console.log(human);
+}
+
+getHumanChoice();
+//  -----
+
