73219b877aba8d3e6c1bbb367bd8a51b67cb76d5
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-21 21:41:45 +0100

    Finished basic UI

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Computer
 function getComputerChoice(computer) {                  
     computer = Math.floor(Math.random() * 3);           // Create a random number between 0 and 2
 
@@ -12,75 +11,64 @@ function getComputerChoice(computer) {
     computerSelectionDisplay.textContent = (`Computer : ` + computer);
     return computer;
 }
-//  -----
 
-//Human
-/*function getHumanChoice() {                                                                     //Get player choice
-    let answer = prompt(`Please choose between Rock, Paper and Scissors.`).toLowerCase();        //Let user choose his play
-        if (answer === `rock` || answer === `paper` || answer === `scissors`) {
-            return answer
-        }
-        else {
-            alert(`Incorrect choice. Please choose a valid input`);
-            return getHumanChoice();
-        }
-}*/
-//  -----
-
-//Score tracker
 let humanScore = 0;
 let computerScore = 0;
-let totalScore = humanScore + computerScore;
-console.log(totalScore);
-//  -----
 
-//Round script
+function winner() {
+        let winner = Math.max(humanScore, computerScore);
+            if (winner === humanScore) {
+                alert(`The player win !`);
+                humanScore = 0;
+                computerScore = 0;
+            } else {
+                alert(`The computer win !`);
+                humanScore = 0;
+                computerScore = 0;
+            }
+}
+
 function playGame() {
     function playRound (humanChoice, computerChoice) {
         playerSelectionDisplay.textContent = (`Player : ` + humanChoice);
-        let totalScore = humanScore + computerScore;
-        console.log(totalScore);
 
-        if (totalScore === 5) {
-            let winner = Math.max(humanScore, computerScore);
-            //let winnerName = Math.max(humanScore, computerChoice);
-                if (winner === humanScore) {
-                    alert(`The player win with ${winner} points !`);
-                } else {
-                    alert(`The computer win with ${winner} points !`);
-                }
-
-        } else if (humanChoice === computerChoice) {                                           // I'm going to hell for this
+        if (humanChoice === computerChoice) {                                           
             roundResult.textContent = (`"It's a draw"`);
-            score.textContent = (`The score is Computer : ` + computerScore + `  human : ` + humanScore);
+            score.textContent = (`The score is Computer : ` + computerScore + `  player : ` + humanScore);
         } else  {
             if (computerChoice === `rock` && humanChoice === `scissors`) {               
-                computerScore ++;                                                       //Increment score
+                computerScore++;                                                       
                 roundResult.textContent = (`You lose! ${computerChoice} beats ${humanChoice}.`);
-                score.textContent = (`The score is Computer : ` + computerScore + `  human : ` + humanScore);
+                score.textContent = (`The score is Computer : ` + computerScore + `  player : ` + humanScore);
+                totalScore = Math.max(humanScore, computerScore);
+                console.log(totalScore);
+                if (totalScore === 5) {winner()};
             } else if (computerChoice === `scissors` && humanChoice === `paper`) {       
-                computerScore ++;                                                       //Increment score
+                computerScore++;                                                       
                 roundResult.textContent = (`You lose! ${computerChoice} beats ${humanChoice}.`);
-                score.textContent = (`The score is Computer : ` + computerScore + `  human : ` + humanScore);
+                score.textContent = (`The score is Computer : ` + computerScore + `  player : ` + humanScore);
+                totalScore = Math.max(humanScore, computerScore);
+                console.log(totalScore);
+                if (totalScore === 5) {winner()};
             } else if (computerChoice === `paper` && humanChoice === `rock`) {
-                computerScore ++;                                                       //Increment score
+                computerScore++;                                                       
                 roundResult.textContent = (`You lose! ${computerChoice} beats ${humanChoice}.`);
-                score.textContent = (`The score is Computer : ` + computerScore + `  human : ` + humanScore);
+                score.textContent = (`The score is Computer : ` + computerScore + `  player : ` + humanScore);
+                totalScore = Math.max(humanScore, computerScore);
+                console.log(totalScore);
+                if (totalScore === 5) {winner()};
             } else {
-                humanScore ++;                                                          //Increment player score
+                humanScore++;                                                          
                 roundResult.textContent = (`You win! ${humanChoice} beats ${computerChoice}.`);
-                score.textContent = (`The score is Computer : ` + computerScore + `  human : ` + humanScore);
+                score.textContent = (`The score is Computer : ` + computerScore + `  player : ` + humanScore);
+                totalScore = Math.max(humanScore, computerScore);
+                console.log(totalScore);
+                if (totalScore === 5) {winner()};
             }
         }
     }
-    //alert(`The final score is\n Computer : ` + computerScore + `  human : ` + humanScore )
     playRound(humanSelection, computerSelection);
 }
-
-
-//let humanSelection = getHumanChoice();
-
-
 
 const rockBtn = document.querySelector ("#rockBtn");
 const paperBtn = document.querySelector ("#paperBtn");
@@ -111,27 +99,3 @@ const score = document.querySelector ("#score");
 const computerSelectionDisplay = document.querySelector ("#computerSelection");
 const playerSelectionDisplay = document.querySelector ("#playerSelection");
 const roundResult = document.querySelector ("#roundResult");
-
-
-/*humanSelection = getHumanChoice();                                                      //Straying further and further from the light
-computerSelection = getComputerChoice();
-playGame ();
-console.log(`Computer score : ` + computerScore, `human score : ` + humanScore);
-
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playGame ();
-console.log(`Computer score : ` + computerScore, `human score : ` + humanScore);
-
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playGame ();
-console.log(`Computer score : ` + computerScore, `human score : ` + humanScore);
-
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playGame ();
-console.log(`Computer score : ` + computerScore, `human score : ` + humanScore);
-*/
-
-
